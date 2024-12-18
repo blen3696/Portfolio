f49350363fae2e4e076c4168b50ce1be8afc6bc1
Blen Redwan <blenredwan@gmail.com>
2024-12-18 22:37:54 +0300

    last commit

--- script/index.js
+++ script/index.js
@@ -41,34 +41,25 @@ menuIcon.onclick = () =>{
 }
 
 
-//scroll section 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () =>{
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id')
 
-        if(top >= offset && top < offset+height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active')
-            })
-            //active sections effect on scroll
-            sec.classList.add('show-animate');
-        }
-        else {
-            sec.classList.remove('show-animate');
-        }
-    })
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100)
-
+    window.onload = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 100;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+    
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            }
+        });
+    };
+    
 //remove icons 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
 
-}
